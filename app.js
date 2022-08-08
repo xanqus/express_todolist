8aa3a65598ca8c96f0a619c3d85ab79f4550c541
xanqus <sbk98031816@gmail.com>
2022-08-08 18:34:46 +0900

    todolist drag&drop

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
 // app.js
-import express, { query } from 'express'
-import mysql from 'mysql2/promise'
-import cors from 'cors'
-import axios from 'axios'
+import express, { query } from "express";
+import mysql from "mysql2/promise";
+import cors from "cors";
+import axios from "axios";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const port = 4000
+const port = 4000;
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'sbsst',
-  password: 'sbs123414',
-  database: 'a9',
+  host: "localhost",
+  user: "sbsst",
+  password: "sbs123414",
+  database: "a9",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-})
+});
 
 const getData = async () => {
-  const data = await axios.get('http://localhost:3000/todos')
-}
+  const data = await axios.get("http://localhost:3000/todos");
+};
 
-app.get('/todos/:id/:contentId', async (req, res) => {
+app.get("/todos/:id/:contentId", async (req, res) => {
   // params 여러개 받기
   const data = {
     todos: {
       id: req.params.id,
       contentId: req.params.contentId,
     },
-  }
+  };
 
   const {
     todos: { id, contentId },
-  } = data
-})
+  } = data;
+});
 
-app.get('/todos', async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM todo ORDER BY id DESC')
+app.get("/todos", async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM todo ORDER BY id DESC");
   //getData()
-  res.json(rows)
-})
+  res.json(rows);
+});
 
-app.post('/todos', async (req, res) => {
+app.post("/todos", async (req, res) => {
   const {
     body: { text },
-  } = req
+  } = req;
   await pool.query(
     `
   INSERT INTO todo
@@ -57,19 +57,19 @@ app.post('/todos', async (req, res) => {
   text = ?;
   `,
     [text]
-  )
+  );
   const [[rows]] = await pool.query(`
   SELECT *
   FROM todo
   ORDER BY id
   DESC LIMIT 1
-  `)
-  res.json(rows)
-})
+  `);
+  res.json(rows);
+});
 
-app.get('/todos/:id/', async (req, res) => {
+app.get("/todos/:id/", async (req, res) => {
   //const id = req.params.id;
-  const { id } = req.params
+  const { id } = req.params;
 
   const [rows] = await pool.query(
     `
@@ -78,20 +78,20 @@ app.get('/todos/:id/', async (req, res) => {
   WHERE id = ?
   `,
     [id]
-  )
+  );
   if (rows.length === 0) {
     res.status(404).json({
-      msg: 'not found',
-    })
-    return
+      msg: "not found",
+    });
+    return;
   }
 
-  res.json(rows[0])
-})
+  res.json(rows[0]);
+});
 
-app.patch('/todos/:id', async (req, res) => {
-  const { id } = req.params
-  const { perform_date, text } = req.body
+app.patch("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  const { perform_date, text } = req.body;
 
   const [rows] = await pool.query(
     `
@@ -100,26 +100,26 @@ app.patch('/todos/:id', async (req, res) => {
     WHERE id = ?
     `,
     [id]
-  )
+  );
 
   if (rows.length === 0) {
     res.status(404).json({
-      msg: 'not found',
-    })
+      msg: "not found",
+    });
   }
 
   if (!perform_date) {
     res.status(400).json({
-      msg: 'perform_date required',
-    })
-    return
+      msg: "perform_date required",
+    });
+    return;
   }
 
   if (!text) {
     res.status(400).json({
-      msg: 'text required',
-    })
-    return
+      msg: "text required",
+    });
+    return;
   }
 
   const [rs] = await pool.query(
@@ -130,27 +130,27 @@ app.patch('/todos/:id', async (req, res) => {
     WHERE id = ?
     `,
     [perform_date, text, id]
-  )
+  );
 
   res.json({
     msg: `${id}번 할일이 수정되었습니다.`,
-  })
-})
+  });
+});
 
-app.patch('/todos/check/:id', async (req, res) => {
-  const { id } = req.params
+app.patch("/todos/check/:id", async (req, res) => {
+  const { id } = req.params;
   const [[rows]] = await pool.query(
     `
     SELECT *
   FROM todo where id = ?
   `,
     [id]
-  )
+  );
   if (!rows) {
     res.status(404).json({
-      msg: 'not found',
-    })
-    return
+      msg: "not found",
+    });
+    return;
   }
   await pool.query(
     `
@@ -160,12 +160,50 @@ app.patch('/todos/check/:id', async (req, res) => {
   `,
 
     [!rows.checked, id]
-  )
-  res.send(id)
-})
+  );
+  res.send(id);
+});
 
-app.delete('/todos/:id', async (req, res) => {
-  const { id } = req.params
+app.patch("/todos/swap/:id", async (req, res) => {
+  const { id } = req.params;
+  const { targetId } = req.body;
+  if (!id) {
+    res.status(400).json({
+      msg: "id required",
+    });
+    return;
+  }
+  if (!targetId) {
+    res.status(400).json({
+      msg: "id targetId",
+    });
+    return;
+  }
+
+  await pool.query(
+    `
+  UPDATE todo a
+  INNER JOIN todo b ON a.id != b.id
+   SET a.reg_date = b.reg_date,
+       a.perform_date = b.perform_date,
+       a.checked = b.checked,
+       a.text = b.text
+  WHERE a.id IN (? , ?) AND b.id IN (? , ?)
+  `,
+    [targetId, id, id, targetId]
+  );
+  const [updatedTodos] = await pool.query(
+    `
+    SELECT *
+    FROM todo
+    ORDER BY id DESC
+    `
+  );
+  res.json(updatedTodos);
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  const { id } = req.params;
 
   const [[todoRow]] = await pool.query(
     `
@@ -173,25 +211,25 @@ app.delete('/todos/:id', async (req, res) => {
     FROM todo
     WHERE id = ?`,
     [id]
-  )
+  );
 
   if (todoRow === undefined) {
     res.status(404).json({
-      msg: 'not found',
-    })
-    return
+      msg: "not found",
+    });
+    return;
   }
 
   const [rs] = await pool.query(
     `DELETE FROM todo
     WHERE id = ?`,
     [id]
-  )
+  );
   res.json({
     msg: `${id}번 할일이 삭제되었습니다.`,
-  })
-})
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
